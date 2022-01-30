1a645787f04beb6a883b35a11a4c9389c539736b
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-01-29 22:30:40 -0500

    commit tree f8e8e570d0b52652db36577eb1abaa900c0ab614

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+const returnLastTwoDrivers = (drivers) => drivers.slice(drivers.length - 2);
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = (Int) =>
+  function (fare) {
+    return fare * Int;
+  };
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+const selectDifferentDrivers = (drivers, cbFxn) => cbFxn(drivers);
